14249234aab2397959cbf2d16b50cf5a632d2a14
vinay1723 <144577819+vinay1723@users.noreply.github.com>
2025-06-18 16:53:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let res = "";
+const submit = document.querySelector('button[type="submit"]');
+let form = document.getElementsByTagName('form')[0];
+const inputs = document.getElementsByTagName('input');
+
+form.addEventListener('submit',(e) => {
+	e.preventDefault();
+	for (let input of inputs) {
+		res += (input.getAttribute('name')+ ":" + " "+ input.value + " ");
+	}
+	alert(res);
+})
